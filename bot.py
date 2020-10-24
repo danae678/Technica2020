30a0f64977c31c44e96148af809bda87e9c896d0
Danae Payne <Danae@Danaes-MacBook.fios-router.home>
2020-10-24 13:22:09 -0400

    added variables for api tokens and gitignore

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
 #code for the bot
+import tweepy, time, sys
+from os import environ
+
+#these are where the API Tokens that Morgan set up will go
+CONSUMER_KEY = environ['CONSUMER_KEY'] 
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+
